c83d6941ca9374b5e99f1961870615b2ed9c754d
Omi <pb2986@nyu.edu>
2025-03-02 20:27:12 -0500

    robotCode

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let cols, rows;
 let size = 10;
 let font;
 let threshold = 100;
+let angle = 0;
 
 function preload() {
   font = loadFont("ibm.ttf");
@@ -17,20 +18,19 @@ function setup() {
 
 function draw() {
   background(0);
+  angle += 0.02; // Increment the angle for animation
 
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      let x = size / 2 + i * size;
-      let y = size / 2 + j * size;
+      let x = size / 2 + i * size + 20 * cos(angle + i * 0.1);
+      let y = size / 2 + j * size + 20 * sin(angle + j * 0.1);
       let d = dist(x, y, width / 2, height / 2);
-      fill(40);
-
       fill(255);
       textFont(font);
       if (d > threshold) {
         text("+", x, y);
       } else {
-        text(".", x, y);
+        text("+", x, y);
       }
     }
   }
